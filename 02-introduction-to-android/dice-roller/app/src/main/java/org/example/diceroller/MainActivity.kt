86154ac7c8f3be7641f8800c01c431cb533bab58
Ivan Shchitov <polotenchik1992@gmail.com>
2020-01-26 21:01:13 +0300

    Add the dice-roller app example

---
+++ 02-introduction-to-android/dice-roller/app/src/main/java/org/example/diceroller/MainActivity.kt
@@ -0,0 +1,34 @@
+package org.example.diceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var diceImage: ImageView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.setOnClickListener {
+            rollDice()
+        }
+        diceImage = findViewById(R.id.dice_image)
+    }
+
+    private fun rollDice() {
+        val drawableRes = when (Random.nextInt(1, 6)) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else ->  R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableRes)
+    }
+}
