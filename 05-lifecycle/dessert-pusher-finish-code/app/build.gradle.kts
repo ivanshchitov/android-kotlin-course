b68aafafe48bab668774c641d61287bde9984254
Ivan Shchitov <ivan.shchitov@fruct.org>
2026-01-27 00:01:20 +0300

    Convert Groovy files to Kotlin files in the Dessert Pusher example for 5th lesson

---
+++ 05-lifecycle/dessert-pusher-finish-code/app/build.gradle.kts
@@ -0,0 +1,41 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.example.android.dessertpusher"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.example.android.dessertpusher"
+        minSdk = 28
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    buildFeatures {
+        dataBinding = true
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+dependencies {
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+    implementation("androidx.appcompat:appcompat:1.7.1")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.1")
+}
